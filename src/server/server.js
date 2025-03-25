021176c00851babcc84a0be0b055ba3dbf2498f9
Jook <jookcucuta@gmail.com>
2025-03-25 08:22:44 -0500

    Feat: changes into deploying

--- src/server/server.js
+++ src/server/server.js
@@ -1,2 +1,11 @@
-"use strict";
-console.log("Prueba");
+import express from 'express'
+import { PORT } from '../config/config.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+  console.log('nadasssss')
+})
+
+app.listen(PORT)
+console.log('Server on port', PORT)
