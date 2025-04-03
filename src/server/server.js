142efaabd3c5614f7380fe78d1bc20006b6a5f40
GambMoli <g.molina@nelumbo.com.co>
2025-04-03 11:44:40 -0500

    Changes

--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from '../config/config.js';
 import routerUser from '../routes/user/index.js';
 import routerLicense from '../routes/license/index.js';
 
 const app = express();
 
-// Lista de orígenes permitidos
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://sistema-firmas-production.up.railway.app'
-];
-
-// Middleware de CORS
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
-
-  // Manejo de preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-  next();
-});
+app.use(cors())
 
 // Middleware para parsear JSON
 app.use(express.json());
